97572fe8adc0c152aeee9a820fc877e025155058
Ngoc Don <kaitoukid2701@gmail.com>
2016-03-26 23:00:05 +0700

    Benchmark Encryption

--- file-worker.js
+++ file-worker.js
@@ -5,22 +5,22 @@ onmessage = function (msg) {
 	if (msg.data.type == 'encrypt'){
 		var file = msg.data.file;
 
-		/** // FireFox does not support FileReader in Web Worker. => use FileReaderSync.
-		 * var reader = new FileReader();
-		 * reader.onload = function (evt) {
-		 *	var encrypted = CryptoJS.AES.encrypt(evt.target.result, msg.data.key).toString();
-		 *	postMessage({
-		 *		cipher: encrypted
-		 *	})
-		 * }
-		 * reader.readAsDataURL(file);
-		 */
+		// FireFox does not support FileReader in Web Worker. => use FileReaderSync.
+		// var reader = new FileReader();
+		// reader.onload = function (evt) {
+		// 	var encrypted = CryptoJS.AES.encrypt(evt.target.result, msg.data.key).toString();
+		// 	postMessage({
+		// 		cipher: encrypted
+		// 	})
+		// }
+		// reader.readAsDataURL(file);
+		
 		var reader = new FileReaderSync();
 		var dataURL = reader.readAsDataURL(file);
 		var encrypted = CryptoJS.AES.encrypt(dataURL, msg.data.key).toString();
 		postMessage({
 			cipher: encrypted
-		})
+		});
 	}
 	else if (msg.data.type = 'decrypt'){
 		var cipher = msg.data.cipher;
@@ -28,6 +28,6 @@ onmessage = function (msg) {
 		var decrypted = CryptoJS.AES.decrypt(cipher, key).toString(CryptoJS.enc.Latin1);
 		postMessage({
 			dataURL: decrypted
-		})
+		});
 	}
 }
