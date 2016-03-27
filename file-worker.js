f6c5c270d6a7224b3da031d6b21e31a84f7f803c
Ngoc Don <kaitoukid2701@gmail.com>
2016-03-28 00:06:21 +0700

    Recover file-worker.js

---
+++ file-worker.js
@@ -0,0 +1,33 @@
+importScripts('crypto-js/build/rollups/aes.js');
+importScripts('crypto-js/build/components/enc-base64-min.js');
+
+onmessage = function (msg) {
+	if (msg.data.type == 'encrypt'){
+		var file = msg.data.file;
+
+		// FireFox does not support FileReader in Web Worker. => use FileReaderSync.
+		// var reader = new FileReader();
+		// reader.onload = function (evt) {
+		// 	var encrypted = CryptoJS.AES.encrypt(evt.target.result, msg.data.key).toString();
+		// 	postMessage({
+		// 		cipher: encrypted
+		// 	})
+		// }
+		// reader.readAsDataURL(file);
+		
+		var reader = new FileReaderSync();
+		var dataURL = reader.readAsDataURL(file);
+		var encrypted = CryptoJS.AES.encrypt(dataURL, msg.data.key).toString();
+		postMessage({
+			cipher: encrypted
+		});
+	}
+	else if (msg.data.type = 'decrypt'){
+		var cipher = msg.data.cipher;
+		var key = msg.data.key;
+		var decrypted = CryptoJS.AES.decrypt(cipher, key).toString(CryptoJS.enc.Latin1);
+		postMessage({
+			dataURL: decrypted
+		});
+	}
+}
